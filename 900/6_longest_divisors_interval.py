5d1b75efc839e7b16330c05ea542a11a05a6ced1
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-01-15 23:13:23 +0530

    Solution for 'longest divisors interval'

---
+++ 900/6_longest_divisors_interval.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    count = 0
+    for i in range(1,n+1):
+        if n % i == 0:
+            count += 1
+        else:
+            break
+    print(count)
