63f03b67495b5b048b9a35c3847b1a15362e7b9e
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-01-18 11:25:36 +0530

    Solution for 'minimum LCM' udated

--- 1000/9_minimum_LCM.py
+++ 1000/9_minimum_LCM.py
@@ -4,14 +4,11 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     n = int(input())
-    minn = float('inf')
-    ans = [0,0]
-    for i in range(1,n//2+1):
-        a , b = i , n - i
-        lcm = (a*b)//math.gcd(a,b)
-        if lcm < minn:
-            minn = lcm
-            ans[0] = a
-            ans[1] = b   
-    print(ans[0], ans[1])
+    best = 1
+    for i in range(2,math.isqrt(n) + 1):
+        if n % i == 0:
+            best = n // i
+            break
+    print(best, n- best)
+
 
