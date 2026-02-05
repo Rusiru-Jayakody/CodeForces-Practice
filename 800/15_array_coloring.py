1f56251554a5222be2a7ac0047e6effbae4f6065
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-02-05 17:08:21 +0530

    Solution for 'array coloring'

---
+++ 800/15_array_coloring.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    count = 0
+    for c in arr:
+        if c%2 == 1:
+            count += 1
+    if count % 2 == 0:
+        print("YES")
+    else:
+        print("NO")
