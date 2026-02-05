de4649bfe4b5849900f775ff4cd7e4e112f8230c
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2026-02-05 08:51:50 +0530

    Solution for 'not dividing'

---
+++ 900/11_not_dividing.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    for i in range(n):
+        if arr[i] == 1:
+            arr[i] += 1
+
+    for i in range(n-1):
+        if arr[i] == 1:
+            arr[i] += 1
+        
+        if arr[i+1] % arr[i] == 0:
+            arr[i+1] += 1
+         
+    print(*arr)
